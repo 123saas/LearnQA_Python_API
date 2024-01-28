c3cc0ecd12000f247a6b998148ccde8bb4298e4e
sa <pd.dp.83@mail.ru>
2024-01-28 17:16:23 +0300

    Ex6: Длинный редирект

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello from Amalia")
+# -*- coding: utf-8 -*-
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True) # allow_redirects=True означает, что сервер пытается перенаправить нас на новую урл, мы следуем за них пока не достигнем конечной точки. allow_redirects=False мы не пойдем на рекдирект и в ответе будут только данные первого запроса. True - дефолтное значение
+all_responses = response.history # чтобы узнать куда именно нас перенаправили, мы можем вызвать фукцию history. она вернет массив всех
+for i in range(len(all_responses)): # цикл от 0 до количества редиректов (len(all_responses))
+    url_response = response.history[i] # в переменную кладем урл
+    print(url_response.url)
