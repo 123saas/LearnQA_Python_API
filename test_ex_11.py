7663d220a90fd3fa6fd3a19e103ddbf2ceb9a3fa
sa <pd.dp.83@mail.ru>
2024-02-04 00:36:32 +0300

    Ex11: Тест запроса на метод cookie

---
+++ test_ex_11.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import requests
+class TestCookie:
+    def test_method_cocckie(self):
+        url = "https://playground.learnqa.ru/api/homework_cookie"
+        response = requests.get(url)
+        cookie_dict = dict(response.cookies) # отображение cookie в виде словаря
+        print(cookie_dict)
+        assert "HomeWork" in cookie_dict, f"Ключ 'HomeWork' отсутствует" # проверка, что есть ключ HomeWork
+        expected_response_text = "hw_value" # ожидаемый текст
+        actual_response_text = cookie_dict['HomeWork'] # фактический текст
+        assert expected_response_text == actual_response_text, f"Фактический текст не совпадает с ожидаемым"
